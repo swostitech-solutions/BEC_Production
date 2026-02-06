45f1ab18962dcfb06400a13c419f7d94fd0b8060
Dhruva-D <dhruvad575@gmail.com>
2026-02-06 22:43:34 +0530

    Remove automatic superuser creation - will create manually via shell

--- build.sh
+++ build.sh
@@ -18,15 +18,4 @@ python manage.py collectstatic --no-input
 echo "Running database migrations..."
 python manage.py migrate --no-input
 
-echo "Creating superuser if not exists..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-if not User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists():
-    User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}');
-    print('Superuser created successfully!');
-else:
-    print('Superuser already exists.');
-"
-
 echo "Build completed successfully!"
